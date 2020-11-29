7383a4c570c0d1daca6deca82758a0f930b2156b
root <root@Loweex.pulseheberg.com>
2020-11-29 18:46:55 +0100

    im sad I didn't versioned ;-;

--- cogs/spc.py
+++ cogs/spc.py
@@ -1,6 +1,7 @@
 import discord
 import urllib, json
 import DiscordUtils
+from math import ceil
 from discord.ext import commands
 
 
@@ -23,52 +24,59 @@ class MembersCog(commands.Cog):
                                    color=3903947)
         processing.set_thumbnail(url="https://cdn.discordapp.com/emojis/407266065660510218.gif?v=1")
         await ctx.send(embed=processing, delete_after=2)
-        
-
-        await ctx.send(embed=embed)
-
-    @commands.command()
-    async def paginate(self, ctx):
-        embed1 = discord.Embed(color=ctx.author.color).add_field(name="Example", value="Page 1")
-        embed2 = discord.Embed(color=ctx.author.color).add_field(name="Example", value="Page 2")
-        embed3 = discord.Embed(color=ctx.author.color).add_field(name="Example", value="Page 3")
         paginator = DiscordUtils.Pagination.CustomEmbedPaginator(ctx, remove_reactions=True)
+        paginator.add_reaction('⏪', "first")
         paginator.add_reaction('◀️', "back")
         paginator.add_reaction('▶️', "next")
-        embeds = [embed1, embed2, embed3]
-        await paginator.run(embeds)
+        paginator.add_reaction('⏩', "last")
+        await paginator.run(fetch_data())
 
-def embed_creator(embed_name, embed_num, username, play_output, number):
-    embed_name.add_field(name=username, 
-                        value=play_output, 
-                        inline=False)
-    embed_name.set_footer(icon_url="https://cdn.discordapp.com/icons/513160124219523086/a_3dc65aae06b2cf7bddcb3c33d7a5ecef.gif?size=128",
-                    text=f"{embed_num} / {number}")
-
-def fecth_data(num):
+def fetch_data():
     with open("/home/nepmia/Myazu/db/db.json") as api:
-            data = json.load(api)
-            default = data.get("_default")
-            count = 0
-            embed = discord.Embed(title=f'**SPC** Last week online time',
-                                  color=3903947)
-            for number in default:
-                count += 1
-                track = default.get(number)
-                username = track.get("username")
-                ur2 = f"https://api.wynncraft.com/v2/player/{username}/stats"
-                u2 = urllib.request.urlopen(ur2)
-                api_2 = json.loads(u2.read().decode())
-                data2 = api_2.get("data")
-                for item in data2:
-                            meta = item.get("meta")
-                            playtime = meta.get("playtime")
-                            play_in = track.get("playtime")
-                            play_minus = playtime - play_in
-                            play_mult = (play_minus * 4.7) // 60
-                            play_output = f"played {float(play_mult):g} hours"
-                            if count > num:
-                                embed_creator(embed_name=embed, embed_num=num, username=username, play_output=play_output, number=number)
+        data = json.load(api)
+    db = data.get("_default")
+    embeds = []
+    for i, elt in enumerate(db):
+        user_data = db.get(elt)
+        username = user_data.get("username")
+        ur2 = f"https://api.wynncraft.com/v2/player/{username}/stats"
+        u2 = urllib.request.urlopen(ur2)
+        api_2 = json.loads(u2.read().decode())
+        data2 = api_2.get("data")
+        for item in data2:
+            meta = item.get("meta")
+            playtime = meta.get("playtime")
+            play_in = user_data.get("playtime")
+            play_minus = playtime - play_in
+            play_mult = (play_minus * 4.7) / 60
+            play_output = f"played {round(play_mult, 1)} hours"
+            embeds.append(
+                {
+                    "username": username, 
+                    "play_output": play_output, 
+                })
+    return embed_creator(embeds)
+    
+            
+def chunker(seq, size):
+    for i in range(0, len(seq), size):
+        yield seq[i:i+size]
+
+def embed_creator(embeds):
+    pages = []
+    current_page = None
+    for i, chunk in enumerate(chunker(embeds, 10)):
+        current_page = discord.Embed(title=f'**SPC** Last week online time',color=3903947)
+        for elt in chunk:
+            current_page.add_field(name=elt.get("username"), value=elt.get("play_output"), inline=False)
+        current_page.set_footer(
+            icon_url="https://cdn.discordapp.com/icons/513160124219523086/a_3dc65aae06b2cf7bddcb3c33d7a5ecef.gif?size=128", 
+            text=f"{i + 1} / {ceil(len(embeds) / 10)}"
+            )
+        pages.append(current_page)
+        current_page = None
+    return pages
+
 
 def setup(bot):
     bot.add_cog(MembersCog(bot))
--- db/api_fetch.py
+++ db/api_fetch.py
@@ -1,5 +1,4 @@
 import urllib.request, json
-import os
 from shutil import copyfile
 from termcolor import colored
 from tinydb import TinyDB, Query
@@ -13,7 +12,8 @@ def api_get():
             api_1 = json.loads(u1.read().decode())
             count = 0
             if members := api_1.get("members"):
-                print(colored("[Myazu]","cyan"), colored("Got expecteded answer, starting saving process.", "white"))
+                print(colored("[Myazu]","cyan"),
+                      colored("Got expecteded answer, starting saving process.", "white"))
                 for member in members:
                     nick = member.get("name")
                     ur2 = f"https://api.wynncraft.com/v2/player/{nick}/stats"
@@ -23,28 +23,35 @@ def api_get():
                     for item in data:
                             meta = item.get("meta")
                             playtime = meta.get("playtime")
-                            print(colored("[Myazu]","cyan"), colored("Saving playtime for player", "white"), colored(f"{nick}...","green"))
+                            print(colored("[Myazu]","cyan"),
+                                  colored("Saving playtime for player", "white"),
+                                  colored(f"{nick}...","green"))
                             db.insert({"username": nick, "playtime": playtime})
                             count += 1
             else: 
-                print(colored("[Myazu]","cyan"), colored("Unexpected answer from WynncraftAPI [ERROR 1]", "white"))
+                print(colored("[Myazu]","cyan"), 
+                      colored("Unexpected answer from WynncraftAPI [ERROR 1]", "white"))
     except:
-        print(colored("[Myazu]","cyan"), colored("Unhandled error in saving process [ERROR 2]", "white"))
+        print(colored("[Myazu]","cyan"), 
+              colored("Unhandled error in saving process [ERROR 2]", "white"))
     finally:
-        print(colored("[Myazu]","cyan"), colored(f"Finished saving data for", "white"), colored(f"{count}", "green"), colored("players.", "white"))
+        print(colored("[Myazu]","cyan"),
+              colored(f"Finished saving data for", "white"),
+              colored(f"{count}", "green"), 
+              colored("players.", "white"))
 
-def purge_db():
-    print(colored("[Myazu]","cyan"), colored("Purging database...", "white"))
-    f = open("/home/nepmia/Myazu/db/db.json", "w")
-    f.seek(0)
-    f.truncate()
-    print(colored("[Myazu]","cyan"), colored("Purged database.", "white"))
-    print(colored("[Myazu]","cyan"), colored("Starting api_get process.", "white"))
+# def purge_db():
+#     print(colored("[Myazu]","cyan"), colored("Purging database...", "white"))
+#     f = open("/home/nepmia/Myazu/db/db.json", "w")
+#     f.seek(0)
+#     f.truncate()
+#     print(colored("[Myazu]","cyan"), colored("Purged database.", "white"))
+#     print(colored("[Myazu]","cyan"), colored("Starting api_get process.", "white"))
 
-def deploy_db():
-    print(colored("[Myazu]", "cyan"), colored("Deploying database...", "white"))
-    copyfile("/home/nepmia/Myazu/db/db.json", "/var/lib/docker/volumes/nginx_data/_data/api/spc/db.json")
-    print(colored("[Myazu]", "cyan"), colored("Deployed database.", "white"))
+# def deploy_db():
+#     print(colored("[Myazu]", "cyan"), colored("Deploying database...", "white"))
+#     copyfile("/home/nepmia/Myazu/db/db.json", "/var/lib/docker/volumes/nginx_data/_data/api/spc/db.json")
+#     print(colored("[Myazu]", "cyan"), colored("Deployed database.", "white"))
 
 purge_db()
 api_get()
